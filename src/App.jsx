cfbf318ab2dda20660bc6840c458b7f27221b812
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-06 17:04:29 +0100

    Add Footer component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <Header>
         <h1>Journal</h1>
       </Header>
+      <Footer />
     </>
   );
 }
