5976dce000230d4919a70427a0c886bae4d7ca1f
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-06 17:02:33 +0100

    Add Header component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header>
+        <h1>Journal</h1>
+      </Header>
+    </>
+  );
 }
 
 export default App;
