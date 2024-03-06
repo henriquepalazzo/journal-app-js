75088faa58f88513d5c29edd6bae5d7a716b875e
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-06 17:08:43 +0100

    Add Input Field component.

---
+++ src/Components/InputField/InputField.jsx
@@ -0,0 +1,8 @@
+export default function InputField() {
+  return (
+    <>
+      <label htmlFor="motto">Motto</label>
+      <input type="text" name="motto" className="form-input" />
+    </>
+  );
+}
