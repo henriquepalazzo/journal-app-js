08882c868b11ed990811719b87d1b5665a321d8a
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-11 17:09:38 +0100

    Add logic to uplift entries state and handle insertion of new entries

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { uid } from "uid";
+import { useState } from "react";
 import Entries from "./Components/Entries/Entries";
 import EntryForm from "./Components/EntryForm/EntryForm";
 import Footer from "./Components/Footer/Footer";
@@ -6,15 +8,53 @@ import Header from "./Components/Header/Header";
 import TabBar from "./Components/TabBar/TabBar";
 
 function App() {
+  const initialEntries = [
+    {
+      id: 1000,
+      date: "Feb 5, 2025",
+      motto: "We are in a state of chaos",
+      notes:
+        "Today I learned about React State. It was fun! I can't wait to learn more.",
+    },
+    {
+      id: 999,
+      date: "Feb 4, 2025",
+      motto: "Props, Props, Props",
+      notes:
+        "Today I learned about React Props. Mad props to everyone who understands this!",
+    },
+    {
+      id: 998,
+      date: "Feb 3, 2025",
+      motto: "How to nest components online fast",
+      notes:
+        "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+    },
+    {
+      id: 997,
+      date: "Feb 2, 2025",
+      motto: "I'm a React Developer",
+      notes: "My React-ion when I learned about React: 😍",
+    },
+  ];
+  const [entries, setEntries] = useState(initialEntries);
+
+  function handleAddEntry(newEntry) {
+    const date = new Date().toLocaleDateString("en-us", {
+      dateStyle: "medium",
+    });
+    setEntries([...entries, { ...newEntry, id: uid(), date: date }]);
+  }
+  console.log(entries);
   return (
     <>
       <Header>
         <h1>Journal</h1>
       </Header>
       <main>
-        <EntryForm />
+        <EntryForm onAddEntry={handleAddEntry} />
         <TabBar />
-        <Entries />
+        <Entries entries={entries} />
       </main>
       <Footer />
     </>
--- src/Components/EntryForm/EntryForm.jsx
+++ src/Components/EntryForm/EntryForm.jsx
@@ -3,11 +3,19 @@ import InputField from "../InputField/InputField";
 import TextareaField from "../TextareaField/TextareaField";
 import "./EntryForm.css";
 
-export default function EntryForm() {
+export default function EntryForm({ onAddEntry }) {
+  function onSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const newEntry = Object.fromEntries(formData);
+    console.log(newEntry);
+    onAddEntry(newEntry);
+    event.target.reset();
+  }
   return (
     <div className="entry-form">
       <h2>New Entry</h2>
-      <form action="" className="form">
+      <form onSubmit={onSubmit} className="form">
         <InputField />
         <TextareaField />
         <Button />
