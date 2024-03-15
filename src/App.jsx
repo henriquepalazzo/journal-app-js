51058d1254dafeb3ff6f28d9891045e091379e58
Henrique Palazzo <henriquepalazzo@gmail.com>
2024-03-15 11:37:47 +0100

    Move initial data to a separate file.

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,9 @@ import EntryForm from "./Components/EntryForm/EntryForm";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import TabBar from "./Components/TabBar/TabBar";
+import { initialEntries } from "./data/initialEntries";
 
 function App() {
-  const initialEntries = [
-    {
-      id: 1000,
-      date: "Feb 5, 2025",
-      motto: "We are in a state of chaos",
-      notes:
-        "Today I learned about React State. It was fun! I can't wait to learn more.",
-      isFavorite: false,
-    },
-    {
-      id: 999,
-      date: "Feb 4, 2025",
-      motto: "Props, Props, Props",
-      notes:
-        "Today I learned about React Props. Mad props to everyone who understands this!",
-      isFavorite: false,
-    },
-    {
-      id: 998,
-      date: "Feb 3, 2025",
-      motto: "How to nest components online fast",
-      notes:
-        "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
-      isFavorite: false,
-    },
-    {
-      id: 997,
-      date: "Feb 2, 2025",
-      motto: "I'm a React Developer",
-      notes: "My React-ion when I learned about React: 😍",
-      isFavorite: false,
-    },
-  ];
   const [entries, setEntries] = useState(initialEntries);
 
   function handleAddEntry(newEntry) {
